49df23ea19118d98cab77c0df601ca4e1c41ce8e
NikolasDaltro <nikolasdsantos@gmail.com>
2022-10-05 11:43:37 -0300

    projeto formulario finalizado

--- fundamentos/atividadeWeb/html/exercicios/form/server.js
+++ fundamentos/atividadeWeb/html/exercicios/form/server.js
@@ -6,6 +6,11 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 app.post('/usuarios', (req, resp) =>{
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Usuario Incluido!! </h1>')
+})
+app.post('/usuarios/:id', (req, resp) =>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario Alterado!! </h1>')
 })
 app.listen(3003)
