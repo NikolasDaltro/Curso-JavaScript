07d2b9adfba5ed5ca5053914fdeaa0f7b118fb87
NikolasDaltro <nikolasdsantos@gmail.com>
2022-10-21 20:27:37 -0300

    resolução certa

--- fundamentos/exercicios/exercicio3.js
+++ fundamentos/exercicios/exercicio3.js
@@ -1,9 +1,14 @@
 /*3) Crie uma função que recebe dois parâmetros,
  base e expoente, e retorne a base elevada ao expoente */
 
- const base = 10
- const expoente = 2
- const area =  Math.pow(base, expoente)
- console.log(area)
- 
- 
+ function expoente (base, expoente) {
+    //Podemos fazer de 2 modos
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(expoente(2, 3))
