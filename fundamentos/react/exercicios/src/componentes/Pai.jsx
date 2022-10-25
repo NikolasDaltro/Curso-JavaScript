e211d864eeb14d8e54747d62ca38e5e7d5b57eef
NikolasDaltro <nikolasdsantos@gmail.com>
2022-10-25 15:33:22 -0300

    Pai e Filho 2

--- fundamentos/react/exercicios/src/componentes/Pai.jsx
+++ fundamentos/react/exercicios/src/componentes/Pai.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import Filho from './Filho'
+
+function childrenWithProps(props){
+     return React.Children.map(props.children, child =>{
+        return React.cloneElement(child, { 
+             ...props, ...child.props
+         })
+     })
+}
+
 
 export default props =>
 <div>
     <h1>{props.nome} {props.sobrenome}</h1>
     <h2>Filhos</h2>
     <ul>
-        <Filho nome="Nikolas" sobrenome={props.sobrenome}/>
-        <Filho {...props} />
-        <Filho {...props} nome="Vanda" />
+    {
+    childrenWithProps(props)
+    }
        
     </ul>
 </div>
--- fundamentos/react/exercicios/src/index.js
+++ fundamentos/react/exercicios/src/index.js
@@ -3,9 +3,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDom.render(
     <div>
-    <Pai nome="Nikson" sobrenome="Daltro"/>
+    <Pai nome="Nikson" sobrenome="Daltro">
+      <Filho nome="Nikolas" />
+      <Filho nome="Nikson"  />
+      <Filho nome="Carla"  /> 
+    </Pai>
     </div>
     , document.getElementById('root'))
